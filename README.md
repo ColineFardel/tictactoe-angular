49acd484334fcc931a202c86a261f6a65b06b493
ColineFardel <coline.fardel@gmail.com>
2022-09-01 20:59:39 +0200

    change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tarace
+# tictactoe
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tarace'`, () => {
+  it(`should have as title 'tictactoe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tarace');
+    expect(app.title).toEqual('tictactoe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tarace app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tictactoe app is running!');
   });
 });
